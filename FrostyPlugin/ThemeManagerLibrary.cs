ff59f67dc29a7f689363d2088df3e3ca220a057b
shoushou1106 <100991210+shoushou1106@users.noreply.github.com>
2023-10-15 20:59:41 +0800

    wtf im writing

--- FrostyPlugin/ThemeManagerLibrary.cs
+++ FrostyPlugin/ThemeManagerLibrary.cs
@@ -6,9 +6,6 @@ namespace Frosty.Plugin.ThemeManagerLibrary;
 
 /// <summary>
 /// Interface for Frosty theme
-/// <para>Example: </para>
-/// <code>public class ThemePlugin : <see cref="IFrostyTheme"/></code>
-/// <para>you can copy a example by clicking <see cref="IFrostyTheme"/> and scroll down</para>
 /// </summary>
 public interface IFrostyTheme
 {
@@ -51,70 +48,16 @@ public interface IFrostyTheme
 
     */
 
-    /// <summary>
-    /// Must input a <see cref="IThemeManager"/>
-    /// <para>Example: </para>
-    /// <code>public <see cref="IThemeManager"/> ThemeManager { get => new ExampleThemeManager(); }</code>
-    /// </summary>
-//    /// <example>public IThemeManager ThemeManager { get => new ExampleThemeManager(); }</example>
     IThemeManager ThemeManager { get; }
-
-    /// <summary>
-    /// Must input a name
-    /// <para>Example: </para>
-    /// <code>public string Name { get => "Example Theme"; }</code>
-    /// </summary>
     string Name { get; }
-
-    /// <summary>
-    /// Must input a author
-    /// <para>Example: </para>
-    /// <code>public string Author { get => "Example Author"; }</code>
-    /// </summary>
     string? Author { get; }
-
-    /// <summary>
-    /// Input a icon, can be <see langword="null"/>
-    /// <para><see langword="null"/> Example: </para>
-    /// <code>public object? Icon { get; }</code>
-    /// <para>Example: </para>
-    /// <code>public object Icon { get => TODO; }</code>
-    /// </summary>
     object? Icon { get; }
-
-    /// <summary>
-    /// Input a list of screenshots, can be <see langword="null"/>
-    /// <para><see langword="null"/> Example: </para>
-//    /// <code>public List<object>? Screenshots { get; }</code>
-    /// <para>Example: </para>
-//    /// <code>public List<object>? Screenshots { get => TODO; }</code>
-    /// </summary>
     List<object>? Screenshots { get; }
-
-    /// <summary>
-    /// Input a description, can be <see langword="null"/>
-    /// <para><see langword="null"/> Example: </para>
-    /// <code>public <see href="string" cref="?"/> Description { get; }</code>
-    /// <para>Example: </para>
-    /// <code>public <see href="string" cref="?"/> Description { get => "Example Description"; }</code>
-    /// </summary>
     string? Description { get; }
-
-    /// <summary>
-    /// Input a list of links, can be <see langword="null"/>
-    /// <para><see langword="null"/> Example: </para>
-//    /// <code>public List<string>? Links { get; }</code>
-    /// <para>Example: </para>
-//    /// <code>public List<string>? Links { "https://github.com/CadeEvs/FrostyToolsuite", "https://frostytoolsuite.com/", "https://discord.gg/BXJSBzgc" }</code>
-    /// </summary>
     List<string>? Links { get; }
 
     /// <summary>
-    /// Input a list of <see cref="OSPlatform"/>, keep <see langword="null"/> for all platforms
-    /// <para>All platforms(<see langword="null"/>) Example: </para>
-//    /// <code>public List<OSPlatform>? SupportPlatforms { get; }</code>
-    /// <para>Example: </para>
-//    /// <code>public List<OSPlatform>? SupportPlatforms { get => new() { OSPlatform.Windows, OSPlatform.Linux, OSPlatform.EXAMPLE }; }</code>
+    /// Set to null means support all platforms
     /// </summary>
     List<OSPlatform>? SupportPlatforms { get; }
 }
@@ -132,6 +75,8 @@ public static class ThemeManagerLibrary
             Themes = new();
         }
         
+
+
         IsInitialized = true;
         return true;
     }
